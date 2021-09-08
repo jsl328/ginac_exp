0dfbec66b09991d142b19a160f09a0d0d0f7a740
jsl328 <jsl328@yeah.net>
2021-09-08 14:44:43 +0800

    ginac的基础学习

---
+++ ginac_impl/ginac_impl/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  ginac_impl
+//
+//  Created by jiangsl on 2021/9/8.
+//
+
+#include <iostream>
+
+//#include <iostream>
+//#include <vector>
+//#include <unordered_map>
+//#include "ginac.h"
+#include <ginac/ginac.h>
+using namespace std;
+using namespace GiNaC;
+
+void polynomial_expression(){
+
+    symbol x("x"), y("y");
+    ex PolyInp = 4*pow(x,3)*y + 5*x*pow(y,2) + 3*y- pow(x+y,2) + 2*pow(y+2,2) - 8;
+    ex Poly = PolyInp.expand();
+    for (int i=Poly.ldegree(x);i<=Poly.degree(x);++i)
+    {
+        cout << "The x^" << i << "-coefficient is "<< Poly.coeff(x,i) << endl;
+    }
+    cout << "As polynomial in y: "<< Poly.collect(y) << endl;
+}
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+//    std::cout << "Hello, World!\n";
+    polynomial_expression();
+    return 0;
+}
